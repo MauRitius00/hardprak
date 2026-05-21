34159fd80cdcb1cd1fd3dc3b0349fe4d7101eaa4
fw302 <fw302@students.uni-freiburg.de>
2026-05-21 15:57:17 +0200

    fefe

--- ex_02/task2and3/task2and3.ino
+++ ex_02/task2and3/task2and3.ino
@@ -1,81 +1,106 @@
-
-
-// ------------------------------------------------------------
-//  Task 2 and 3:
-//      Write the body of setTimer1Freq() as specified in the exercise sheet.
-//      This should include timer settings.
-//      Implement the ISR in TIMER1_IRQHandler().
-//      Make necessary changes for setBuzzerFreq()
-// ------------------------------------------------------------
-
 #include <Arduino.h>
 
-bool currenttogglestate = 0;
-void setup() {
-  // Set Pin P0.29 to Output
-  NRF_P0->DIRSET = (1UL << 29);
-  NRF_TIMER1->PRESCALER = 4;
-  NRF_TIMER1->BITMODE = 3;
-  NRF_TIMER1->MODE = 0;
+// ------------------------------------------------------------
+// Task 2 and 3:
+// ------------------------------------------------------------
 
-  NRF_P0->DIRCLR = (1UL << 3);
-  
+volatile bool currentToggleState = false; // for ISR to generate Frequence
+bool buzzerRunning = false; // to work with button task 3
+
+void setTimer1Freq();
+void setBuzzerFreq();
+void setP029(bool high);
+
+void setup() {
+
+  // initialize buzzer (Pin 0.29 as Output)
+  NRF_P0->DIRSET = (1UL << 29);
+  NRF_P0->OUTCLR = (1UL << 29);
+
+  // initialize Button as aktive low
+
+  // set correct binary value in register for button
+  // 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 0 0
+  NRF_P0->PIN_CNF[3] = 12;
 
   setTimer1Freq();
-  NRF_TIMER1->TASKS_START = 1;
-}
 
+  NRF_TIMER1->TASKS_STOP = 1;
+  NRF_TIMER1->TASKS_CLEAR = 1;
+}
 
 void loop() {
   setBuzzerFreq();
 }
 
-
-
 void setTimer1Freq() {
-  // read from timer1 and interrupt
-  NRF_TIMER1->CC[0] = 1000000UL / (2 * 1046UL);
-  NRF_TIMER1->SHORTS = TIMER_SHORTS_COMPARE0_CLEAR_Msk;
+  // Timer setup acording to task 2
+
+  NRF_TIMER1->TASKS_STOP = 1;
+  NRF_TIMER1->TASKS_CLEAR = 1;
+
+
+  NRF_TIMER1->MODE = 0;
+  NRF_TIMER1->BITMODE = 3;
+  NRF_TIMER1->PRESCALER = 4; // 1 MHz
+
+  NRF_TIMER1->CC[0] = 1000000UL / (2UL * 1046UL);
+
+  NRF_TIMER1->SHORTS = 1;
+
+  NRF_TIMER1->EVENTS_COMPARE[0] = 0;
+
   NRF_TIMER1->INTENSET = TIMER_INTENSET_COMPARE0_Msk;
 
+
+  NVIC_ClearPendingIRQ(TIMER1_IRQn);
   NVIC_EnableIRQ(TIMER1_IRQn);
-  
 }
 
-
 void setP029(bool high) {
-  // set Pin P0.29 on 1 or 0 (high or low)
   if (high) {
     NRF_P0->OUTSET = (1UL << 29);
-  }
-  else {
+  } else {
     NRF_P0->OUTCLR = (1UL << 29);
   }
-
 }
 
 void setBuzzerFreq() {
-  if (! (NRF_P0->IN) & (1UL << 3)) {
-  NRF_TIMER1->TASKS_START = 1;
-  NVIC_EnableIRQ(TIMER1_IRQn);
-  }
-  NVIC_DisableIRQ(TIMER1_IRQn);
-  NRF_TIMER1->TASKS_STOP = 1;
-}
+  bool buttonPressed = ((NRF_P0->IN & (1UL << 3)) == 0);
 
+  if (buttonPressed && !buzzerRunning) {
+    currentToggleState = false;
+    setP029(false);
+
+    NRF_TIMER1->EVENTS_COMPARE[0] = 0;
+    NVIC_ClearPendingIRQ(TIMER1_IRQn);
+
+    NRF_TIMER1->TASKS_CLEAR = 1;
+    NRF_TIMER1->TASKS_START = 1;
+
+    buzzerRunning = true;
+  }
+
+  if (!buttonPressed && buzzerRunning) {
+    NRF_TIMER1->TASKS_STOP = 1;
+    NRF_TIMER1->TASKS_CLEAR = 1;
+    NRF_TIMER1->EVENTS_COMPARE[0] = 0;
+
+    setP029(false);
+    currentToggleState = false;
+
+    buzzerRunning = false;
+  }
+}
 
 extern "C" void TIMER1_IRQHandler() {
   if (NRF_TIMER1->EVENTS_COMPARE[0]) {
     NRF_TIMER1->EVENTS_COMPARE[0] = 0;
 
     volatile uint32_t dummy = NRF_TIMER1->EVENTS_COMPARE[0];
-  
-    setP029(currenttogglestate);
-    currenttogglestate = ! currenttogglestate;
+    (void)dummy;
+
+    currentToggleState = !currentToggleState;
+    setP029(currentToggleState);
   }
-  
-
 }
-
-
-
