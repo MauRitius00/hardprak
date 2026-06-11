ee8c75854dd784bd26d293418d16dbd4a71dc5e9
fw302 <fw302@students.uni-freiburg.de>
2026-06-11 13:21:15 +0200

    task1 und task 2

--- ex_03/Task1/Task1.ino
+++ ex_03/Task1/Task1.ino
@@ -1,31 +1,74 @@
 // Task 1: Environmental Light Categorization
 // Complete the implementation according to the task description.
 
+// #include <Adafruit_TinyUSB.h>
+
 // TODO: define necessary constants (e.g., calibration range, thresholds, timing)
+#define analogPin A0
+
+const int BaudRate = 115200;
+const int analogResolution = 12;
+const int LightMin = 50;
+const int LightMax = 3500; // Sensor can only read to ~2500 (350 lux)
+const int SerialDelay = 500; // ms
+
+unsigned long last_blink_ms = 0;  // timestamp of last Execution
 
 // TODO: implement a function that categorizes the normalized value
 const char* categorize(int normalized) {
     // TODO: return "LOW", "MEDIUM", or "HIGH"
+    switch(normalized) {
+        case 0 ... 29:
+            return "LOW";
+            break;
+        case 30 ...69:
+            return "MEDIUM";
+            break;
+        case 70 ... 100:
+            return "HIGH";
+            break;
+        default:
+            return "[ERROR] Value out of Range";
+    }
 }
 
 void setup() {
     // TODO: initialize Serial communication
+    Serial.begin(BaudRate);
+    while (!Serial);
 
     // TODO: configure ADC resolution to 12-bit
-
+    analogReadResolution(analogResolution);
 }
 
 void loop() {
     // TODO: implement non-blocking timing using millis()
     // The system should sample every 500 ms
 
-    // TODO: read raw value from light sensor
 
-    // TODO: clamp the raw value to a calibrated range
+    if (millis() - last_blink_ms >= SerialDelay) {
+        // TODO: read raw value from light sensor
+        int analogValue = analogRead(analogPin);
 
-    // TODO: normalize the value to a 0–100 scale
+        // TODO: clamp the raw value to a calibrated range
+        int sensVal = constrain(analogValue, LightMin, LightMax);
 
-    // TODO: determine the category using the categorize() function
+        // TODO: normalize the value to a 0–100 scale
+        int normalizedVal = map(sensVal, LightMin, LightMax, 0, 100);
+        
 
-    // TODO: print raw value, normalized value, and category to Serial
+        // TODO: determine the category using the categorize() function
+        const char* category = categorize(normalizedVal);
+
+        // TODO: print raw value, normalized value, and category to Serial
+        Serial.print("raw value: ");
+        Serial.print(analogValue);
+        Serial.print(" - normalized value: ");
+        Serial.print(normalizedVal);
+        Serial.print(" - category: ");
+        Serial.println(category);
+
+        
+        last_blink_ms = millis();
+  }
 }
--- ex_03/Task2/Task2.ino
+++ ex_03/Task2/Task2.ino
@@ -1,25 +1,52 @@
 // Task 2: Robust Temperature and Humidity Monitoring
 
+#include <Adafruit_TinyUSB.h>
 #include <DHT.h>
 #include <math.h>
 
 // --- Configuration ---
 // TODO: define sensor pin and type (DHT11)
+#define DHTPIN D7
+#define DHTTYPE DHT11
 
 // --- Objects ---
 // TODO: create DHT sensor instance
 
+
+DHT dht(DHTPIN, DHTTYPE);
+
 // --- Timing ---
 unsigned long lastSample = 0;
+
 // TODO: define sampling interval (2 seconds)
+const int SerialDelay = 2000; // 2000ms = 2sec
+
 
 // --- State Variables ---
 // TODO: store last valid temperature and humidity
+
+float temp = 0.0;
+float humid = 0.0;
+
 // TODO: maintain a failure counter
+int fcounter = 0;
+
+// constants
+const float a = 17.62;
+const float b = 243.12;
+
+const int failurwarningthreshhold = 5;
+
+// helper variables
+float gamm = 0.0f;
+float dewPoint = 0.0f;
 
 // --- Computation ---
 float computeDewPoint(float tempC, float relHum) {
     // TODO: implement Magnus formula using natural logarithm
+    gamm = log(relHum / 100) + ((a * tempC) / (b + tempC));
+
+    return ((b * gamm) / (a - gamm));
 }
 
 void setup() {
@@ -27,19 +54,96 @@ void setup() {
     while (!Serial && millis() < 3000);
 
     // TODO: initialize sensor
+    dht.begin();
 }
 
 void loop() {
     unsigned long now = millis();
-
     // TODO: implement non-blocking sampling (2 s)
+    if (now - lastSample >= SerialDelay) {
+
+    
 
     // TODO: read temperature and humidity
 
+
     // TODO: handle invalid readings (NaN)
+    // THIS solultion should work, but does not
+    /*
+
+    float temponread = dht.readTemperature();
+    float humidonread = dht.readHumidity();
+    if (temponread != temponread || humidonread != humidonread) {
+//  if (isnan(temponread) || isnan(humidonread)) // alternative Variante, aber beide funktionieren nichjt richtig, wurde auch im Forum nachgefragt
+        fcounter ++;
+    }
+    else {
+        fcounter = 0;
+        temp = temponread;
+        humid = humidonread;
+    }
+    */
+
+    // new Solution
+    if (dht.read()) {   // returnse False if nan value apears
+    temp = dht.readTemperature();
+    humid = dht.readHumidity();
+    fcounter = 0;
+    } else {
+        fcounter++;
+    }
+
+
     // - update failure counter (reset on success)
     // - reuse last valid values if needed
 
-    // TODO: compute dew point, print formatted output. If failure count exceeds threshold print a warning. 
+    // TODO: compute dew point, print formatted output. If failure count exceeds threshold print a warning.
+    dewPoint = computeDewPoint(temp, humid);
 
+    // we added the information, that it is using the last valid Measurment, if the
+    // first NaN value apears
+    // if failurwarningthreshhold is exceeded the [WARN] will be added
+    if (fcounter > failurwarningthreshhold) {
+
+        Serial.print("[WARN] DHT failures: ");
+        Serial.println(fcounter);
+        Serial.print("temp: ");
+        Serial.print(temp, 1);
+        Serial.print("°C");
+        Serial.print(" humid: ");
+        Serial.print(humid, 1);
+        Serial.print("%");
+        Serial.print(" dewpoint ");
+        Serial.print(dewPoint, 1);
+        Serial.print("C");
+        Serial.println(" (Using last valid)");
+    }
+
+    else if (fcounter > 0) {
+        Serial.print("temp: ");
+        Serial.print(temp, 1);
+        Serial.print("°C");
+        Serial.print(" humid: ");
+        Serial.print(humid, 1);
+        Serial.print("%");
+        Serial.print(" dewpoint ");
+        Serial.print(dewPoint, 1);
+        Serial.print("C");
+        Serial.println(" (Using last valid)");
+    }
+    else {
+        Serial.print("temp: ");
+        Serial.print(temp, 1);
+        Serial.print("°C");
+        Serial.print(" humid: ");
+        Serial.print(humid, 1);
+        Serial.print("%");
+        Serial.print(" dewpoint ");
+        Serial.print(dewPoint, 1);
+        Serial.println("C");
+    }
+
+
+    lastSample = millis();
+    }
 }
